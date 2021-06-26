b8e17661e8251bd290856d153b116c56b89c19ac
AayushiPastagia <53651953+AayushiPastagia@users.noreply.github.com>
2021-06-26 14:24:09 +0530

    code file

---
+++ Working.py
@@ -0,0 +1,42 @@
+'''
+1. Pick empty square
+2. Try any number
+3. Create function that find picked number is valid
+4. Repeat
+5. Backtrack
+'''
+
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print('------------------------')
+
+        for j in range(len(bo)):
+            if j % 3 == 0 and j != 0:
+                print(' | ', end = "")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end = "")
+
+# 1. Find empty square
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo)):
+            if bo[i][j] == 0:
+                return (i,j) #row, column
+
+find_empty(board)
